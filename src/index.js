ca37fce7227a198b0a4723f0decd042a9a8d9bda
Eric Bishard <ebishard@progress.com>
2019-05-02 01:02:31 -0700

    add todoReducer, ref and  dispatch function

--- src/index.js
+++ src/index.js
@@ -1,24 +1,55 @@
-import React from 'react';
+import React, { useReducer, useRef } from 'react'; // 00: add imports
 import { render } from 'react-dom';
 
 import './style.css';
 import * as constants from './constants';
 
-// const initialState = [...constants.TODOS];
-const initialState = [
-  {id: 1, name: 'Get started', complete: false}
-];
+const initialState = [...constants.TODOS];
+const todosReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO': {
+      return (action.name.length)
+        ? [...state, {
+          id: state.length
+            ? Math.max(...state.map(todo => todo.id)) + 1
+            : 0,
+          name: action.name,
+          complete: false
+        }]
+        : state
+    }
+    default: {
+      return state;
+    }
+  }
+} // 02: add reducer function and switch statement
 
 const Todo = () => {
+  const inputRef = useRef(); // 04: add useRef hook (imperative way of using a ref)
+  const [todos, dispatch] = useReducer(todosReducer, initialState); // 01: add useReducer
+
+  // 05: add dispatch function for addTodo!
+  function addTodo(event) {
+    event.preventDefault();
+    dispatch({
+      type: 'ADD_TODO',
+      name: inputRef.current.value,
+      complete: false
+    });
+    inputRef.current.value = '';
+  }
+
   return (
     <>
+      { /* 03: add inputRef */}
       <div className="todo-input">
-        <form>
-          <input type="search" id="add-todo" placeholder="Add Todo..." />
+        <form onSubmit={addTodo}>
+          <input ref={inputRef} type="search" id="add-todo" placeholder="Add Todo..." />
         </form>
       </div>
       <div className="column-container">
-        {initialState.map((todo) => (
+        { /* 06: Change initalState to todos */}
+        {todos.map((todo) => (
           <div key={todo.id} alt={todo.id} className="column-item">
             <div className="flex-container">
               <div className="todo-name">{todo.name}</div>
